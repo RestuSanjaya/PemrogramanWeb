6076fc864e970765a4f057ba4065fb94e4e82e23
Restu Sanjaya <145826252+RestuSanjaya@users.noreply.github.com>
2024-02-19 14:34:14 +0800

    Add files via upload

---
+++ tugasformlogin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uji Coba Formulir</title> 
+    <link rel="stylesheet" type="text/css" href="loginstyle.css">   
+</head>
+<body>
+    <div class="form-wrapper">
+        <form action="prosesform.php" method="post">
+            <h2>Formulir Pendaftaran</h2>
+            <label for="name">Nama Lengkap:</label>
+            <input type="text" id="name" name="name" placeholder="Masukkan Nama Lengkap Anda" required>
+
+            <label for="NIM">NIM:</label>
+            <input type="number" id="NIM" name="NIM" placeholder="Masukkan NIM Anda" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Masukkan Email Anda" required>
+
+            <!-- Alternating background color submit button -->
+            <input type="submit" class="submit-button" value="Submit">
+        </form>
+    </div>
+</body>
+</html>
